e4144978832483a2479f5b0f30e59546857dc000
Synix <nt.lang2002@gmail.com>
2024-11-25 22:56:02 +0100

    Added Problem100
    Keeping the streak up!

---
+++ Easy/Problem100.java
@@ -0,0 +1,31 @@
+/*
+* Runtime: 0ms -> Beats 100% of users with Java
+* Memory: 41.14MB -> Beats 32.02% of user with Java
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null){
+            return p == null && q == null;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
